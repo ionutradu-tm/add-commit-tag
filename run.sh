aaa6987ffd6a6d7038eed91e94b7eef59e1293a7
Ionut Radu <ionut.radu@tremend.com>
2018-10-18 23:54:34 +0300

    Test

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-echo "Test"
+echo "Test2"
 if [[ $WERCKER_GIT_COMMIT_MESSAGE=~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 
 
