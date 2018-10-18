e3d03c47c270f459abfb6b0a90d4e321a8d95623
Ionut Radu <ionut.radu@tremend.com>
2018-10-18 23:24:10 +0300

    Test

--- run.sh
+++ run.sh
@@ -1,36 +1,7 @@
 #!/bin/sh
 
 
-#tracker-repo
-# broadleaf-admin -api -site
-# fulfilment-layer
-# integration-layer
-# magnolia-cms
-
-# git repo
-# broadleaf-commerce
-# fulfilment-layer
-# integration-layer
-# magnolia-cms
-
-#pushd $WERCKER_SOURCE_DIR
-#WERCKER_GIT_COMMIT_MESSAGE=`git log -1 --pretty='%s'`
-#echo Git commit message: $WERCKER_GIT_COMMIT_MESSAGE
-#popd
-
-if [ -z "$WERCKER_GIT_COMMIT_MESSAGE" ]; then
-   fail "GIT_COMMIT_MESSAGE not set"
-fi
-
-TAG_MAGNOLIA=$(cat $WERCKER_SOURCE_DIR/versions/magnolia-cms.version)
-TAG_BROADLEAF=$(cat $WERCKER_SOURCE_DIR/versions/broadleaf-admin.version)
-TAG_FULFILMENT=$(cat $WERCKER_SOURCE_DIR/versions/fulfilment-layer.version)
-TAG_INTENGRATION=$(cat $WERCKER_SOURCE_DIR/versions/INTEGRATION-layer.version)
-
-REP_MAGNOLIA="magnolia-cms"
-REP_BROADLEAF="broadleaf-commerce"
-REP_FULFILMENT="fulfilment-layer"
-REP_INTEGRATION="integration-layer"
+if [[ $WERCKER_GIT_COMMIT_MESSAGE=~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 
 
 
