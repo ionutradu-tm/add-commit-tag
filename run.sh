cdac988453357d970c9752bde39f24dd9475b28a
Ionut Radu <ionut.radu@tremend.com>
2018-10-20 21:03:12 +0300

    Fix var name

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo "Test3"
+echo $WERCKER_GIT_TAG_COMMIT_MESSAGE
 
-if [[ $WERCKER_GIT_ADD_COMMIT_MESSAGE =~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
+if [[ $WERCKER_GIT_TAG_COMMIT_MESSAGE =~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 #if [[ $WERCKER_ADD_COMMIT_TAG_MESS=~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 #if [[ $WERCKER_ADD_COMMIT_TAG_MESS=~ [0-9]+\.[0-9]+ ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 
