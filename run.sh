7557b8a1f722fbe996ead96953a1b0edd25b2982
Ionut Radu <ionut.radu@tremend.com>
2018-10-20 19:04:00 +0300

    Reset 1

--- run.sh
+++ run.sh
@@ -1,33 +1,5 @@
-#!/bin/bash
+#!/bin/sh
 
-if [ -z "$WERCKER_SSH_TUNNEL_SOURCE_PORT" ]; then
-  fail "You must specify a source port to forward"
-fi
+echo "Test3"
+if [[ $WERCKER_GIT_COMMIT_MESSAGE=~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 
-if [ -z "$WERCKER_SSH_TUNNEL_DESTINATION_HOST" ]; then
-  WERCKER_SSH_TUNNEL_DESTINATION_HOST="127.0.0.1"
-fi
-
-if [ -z "$WERCKER_SSH_TUNNEL_DESTINATION_PORT" ]; then
-  WERCKER_SSH_TUNNEL_DESTINATION_PORT="$WERCKER_SSH_TUNNEL_SOURCE_PORT"
-fi
-
-if [ "$WERCKER_SSH_TUNNEL_CONNECTION_PORT" ]; then
-  SSH_PORT="-p $WERCKER_SSH_TUNNEL_CONNECTION_PORT"
-fi
-
-if [ -z "$WERCKER_SSH_TUNNEL_KEEPALIVE" ]; then
-  WERCKER_SSH_TUNNEL_KEEPALIVE="10"
-fi
-
-SSH_CONNECTION="$WERCKER_SSH_TUNNEL_CONNECTION_STRING"
-echo "SSH Connection: $SSH_CONNECTION $SSH_PORT"
-
-SSH_TUNNEL="$WERCKER_SSH_TUNNEL_SOURCE_PORT:$WERCKER_SSH_TUNNEL_DESTINATION_HOST:$WERCKER_SSH_TUNNEL_DESTINATION_PORT"
-info "Opening tunnel with $SSH_TUNNEL"
-
-if  ! ssh -f -o ExitOnForwardFailure=yes -L "$SSH_TUNNEL" "$SSH_CONNECTION" "$SSH_PORT" sleep $WERCKER_SSH_TUNNEL_KEEPALIVE; then
-  fail "Unable to connect to host"
-fi
-
-success "Port forwarded successfully"
