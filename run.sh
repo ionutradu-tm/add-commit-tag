d4df6005d38af124df509005d805e022549b350a
Ionut Radu <ionut.radu@tremend.com>
2018-10-20 20:33:45 +0300

    Test

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 echo "Test3"
 
-if [[ $WERCKER_ADD_COMMIT_TAG_MESS=~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
+#if [[ $WERCKER_ADD_COMMIT_TAG_MESS=~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
+if [[ $WERCKER_ADD_COMMIT_TAG_MESS=~ [0-9]+\.[0-9]+ ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 
