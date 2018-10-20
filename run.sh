b2cee980bf9890772f29ef3a4741bb40f46520ee
Ionut Radu <ionut.radu@tremend.com>
2018-10-20 20:45:48 +0300

    0.0.1

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 echo "Test3"
 
+if [[ $WERCKER_GIT_ADD_COMMIT_MESSAGE =~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 #if [[ $WERCKER_ADD_COMMIT_TAG_MESS=~ ([0-9]+\.[0-9]+)([^,]*) ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
-if [[ $WERCKER_ADD_COMMIT_TAG_MESS=~ [0-9]+\.[0-9]+ ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
+#if [[ $WERCKER_ADD_COMMIT_TAG_MESS=~ [0-9]+\.[0-9]+ ]]; then echo "version: '${BASH_REMATCH[1]}'"; else echo "no match found"; fi
 
